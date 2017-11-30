26eed22efb8c5d891baf757d23895ea291f2b24d
Ian Fanselow <if497@nyu.edu>
2017-11-29 21:11:14 -0500

    first

---
+++ solveChess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  solveChess
+//
+//  Created by Ian Fanselow on 11/29/17.
+//  Copyright © 2017 Ian Fanselow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
